97f32578a8f12b453e5d1f4238957d3d27c5054d
who asks? <user@localhost>
2026-06-26 22:08:20 -0600

    v0.4.8

--- python-monero-ots.mk
+++ python-monero-ots.mk
@@ -4,7 +4,7 @@
 #
 ################################################################################
 
-PYTHON_MONERO_OTS_VERSION = 0.3.0
+PYTHON_MONERO_OTS_VERSION = 0.4.8
 PYTHON_MONERO_OTS_SITE = $(call github,MoneroSDK,ots-python,v$(PYTHON_MONERO_OTS_VERSION))
 PYTHON_MONERO_OTS_DEPENDENCIES = monero-ots python-cffi
 PYTHON_MONERO_OTS_SETUP_TYPE = setuptools
